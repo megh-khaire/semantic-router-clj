f7bf29d54a933cdd964720b4ef871cb2805e884c
Megh Khaire <meghkhaire@gmail.com>
2024-03-30 00:59:42 -0400

    Add project dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,13 @@
 (defproject semantic-router-clj "0.1.0"
+  :author "Megh Khaire"
   :description "Decision making layer for LLMs in Clojure"
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :main ^:skip-aot semantic-router-clj.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies
+  [[org.clojure/clojure "1.11.2"]
+   [cheshire "5.12.0"]
+   [metosin/malli "0.15.0"]
+   [stencil "0.5.0"]
+   [http-kit "2.7.0"]
+   [net.mikera/core.matrix "0.63.0"]
+   [circleci/bond "0.6.0"]])
